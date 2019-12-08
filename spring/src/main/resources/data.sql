272cf00b1cbe685cbea182d002d246804b8583d9
Ernad Delic <ernad.delic77@gmail.com>
2019-12-08 12:01:33 +0100

    inserted new loguser table into database

--- spring/src/main/resources/data.sql
+++ spring/src/main/resources/data.sql
@@ -2,3 +2,7 @@
 INSERT INTO user(first_name, last_name, age) VALUES("Nate", "Murray", 23);
 INSERT INTO user(first_name, last_name, age) VALUES("John", "Smith", 27);
 INSERT INTO user(first_name, last_name, age) VALUES("Tim", "Handry", 35);
+
+
+INSERT INTO loguser(name,password,roles) VALUES("user", "password", "ROLE_USER");
+INSERT INTO loguser(name,password,roles) VALUES("admin", "admin", "ROLE_ADMIN");
