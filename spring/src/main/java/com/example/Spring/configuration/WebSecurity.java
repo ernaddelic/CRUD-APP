786544fbd2c809f66564b77a4427ea43ecfda73e
ernaddelic <ernad.delic77@gmail.com>
2019-12-14 11:16:03 +0100

    sessionMenagement and addFilterBefore methods added

--- spring/src/main/java/com/example/Spring/configuration/WebSecurity.java
+++ spring/src/main/java/com/example/Spring/configuration/WebSecurity.java
@@ -1,20 +1,27 @@
 package com.example.Spring.configuration;
 
+import com.example.Spring.filter.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     @Autowired
     UserDetailsService userDetailsService;
+
+    @Autowired
+    JwtFilter jwtFilter;
     
     @Override
     @Autowired
@@ -31,13 +38,24 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         .antMatchers(HttpMethod.DELETE, "/users/**").hasRole("ADMIN")
         .and().authorizeRequests().antMatchers(HttpMethod.PUT, "/users/**")
         .hasRole("ADMIN").and().authorizeRequests().antMatchers(HttpMethod.GET, "/users/**")
-        .permitAll().and().httpBasic();
-        http.csrf().disable();    
+        .permitAll().and().authorizeRequests().antMatchers(HttpMethod.POST, "/login/**")
+        .permitAll().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and().httpBasic();
+        http.csrf().disable();
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        
     }
 
     @Bean
     public static NoOpPasswordEncoder passwordEncoder() {
       return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
     }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
 }
 
