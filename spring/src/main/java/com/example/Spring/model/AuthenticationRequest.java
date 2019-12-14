3b80418ae49dde50f0a9b789241c5e8cdad288a7
ernaddelic <ernad.delic77@gmail.com>
2019-12-14 10:58:20 +0100

    class required for storing the username and password received from the client

---
+++ spring/src/main/java/com/example/Spring/model/AuthenticationRequest.java
@@ -0,0 +1,34 @@
+package com.example.Spring.model;
+
+public class AuthenticationRequest {
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public AuthenticationRequest(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public AuthenticationRequest() {
+    }
+
+    
+
+    
+}
