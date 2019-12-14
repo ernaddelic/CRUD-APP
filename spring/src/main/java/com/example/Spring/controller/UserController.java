f1869a979f2b3ed9821491a8d11410c7736b49b6
ernaddelic <ernad.delic77@gmail.com>
2019-12-14 11:11:47 +0100

    POST API/login added to authenticate username and password, and if the credentials are valid jwt is provided to the client

--- spring/src/main/java/com/example/Spring/controller/UserController.java
+++ spring/src/main/java/com/example/Spring/controller/UserController.java
@@ -1,32 +1,53 @@
 package com.example.Spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.*;
-import java.nio.file.AccessDeniedException;
 import java.util.List;
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import com.example.Spring.model.AuthenticationRequest;
+import com.example.Spring.model.AuthenticationResponse;
 import com.example.Spring.model.User;
 import com.example.Spring.service.UserService;
+import com.example.Spring.util.JwtUtil;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class UserController {
 
     @Autowired
+    private AuthenticationManager authMenager;
+
+    @Autowired
     private UserService service;
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Autowired
+    JwtUtil jwtToken;
     
     @GetMapping("/users")
     public List<User> findAll() {
         return service.findAll();
     }
 
-    @GetMapping("/login")
-    public String loginUser(HttpServletRequest request) throws AccessDeniedException {
-        if(request.isUserInRole("USER") || request.isUserInRole("ADMIN")) {
-            return "Successfully logged in";
-        } else {
-            throw new AccessDeniedException("Invalid Credentials");
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> createAuthToken(@RequestBody AuthenticationRequest authRequest) throws Exception {
+        try {
+            authMenager.authenticate(
+            new UsernamePasswordAuthenticationToken(authRequest.getName(), authRequest.getPassword()));   
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect username or password", e);
         }
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authRequest.getName());
+        final String jwt = jwtToken.generateToken(userDetails);
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));    
     }
 
     @GetMapping("/users/{id}")
