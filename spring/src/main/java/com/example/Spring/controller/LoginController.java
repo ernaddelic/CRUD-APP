2de4d345aabb6e01354d74bd54e25cbe97b41a87
ernaddelic <ernad.delic77@gmail.com>
2019-12-19 10:54:15 +0100

    new login controller

---
+++ spring/src/main/java/com/example/Spring/controller/LoginController.java
@@ -0,0 +1,44 @@
+package com.example.Spring.controller;
+
+import com.example.Spring.model.AuthenticationRequest;
+import com.example.Spring.model.AuthenticationResponse;
+import com.example.Spring.util.JwtUtil;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class LoginController {
+
+    @Autowired
+    private AuthenticationManager auth;
+
+    @Autowired
+    JwtUtil jwtUtil;
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> createAuthToken(@RequestBody AuthenticationRequest request) throws Exception {
+        try {
+            auth.authenticate(new UsernamePasswordAuthenticationToken(request.getName(), request.getPassword()));
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect username or password", e);
+        }
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(request.getName());
+        final String jwt = jwtUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }   
+}
