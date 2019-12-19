21724ac76fdd5eb935172357a786870c4910b05a
ernaddelic <ernad.delic77@gmail.com>
2019-12-19 10:46:05 +0100

    new component with registration form

---
+++ angular/src/app/sign-up/sign-up.component.spec.ts
@@ -0,0 +1,35 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SignUpComponent } from './sign-up.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
+
+describe('SignUpComponent', () => {
+  let component: SignUpComponent;
+  let fixture: ComponentFixture<SignUpComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SignUpComponent ],
+      imports: [FormsModule,
+        ReactiveFormsModule,
+        BrowserModule,
+        HttpClientModule,
+        RouterTestingModule]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SignUpComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ angular/src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit} from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { RegistrationService } from '../registration.service';
+import { Router } from '@angular/router';
+
+
+export function matchValue(control: AbstractControl): {[key:string]: boolean}  {
+  if (control.get('password').value !== control.get('passwordConfirm').value) {
+    return {notMatch: true}
+  }
+  return null;
+}
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent implements OnInit {
+  myForm: FormGroup;
+  message: string;
+  name: AbstractControl;
+  password: AbstractControl;
+  passwordConfirm: AbstractControl;
+  displayErorr: boolean;
+  displayMatch: boolean;
+
+  constructor( fb: FormBuilder,
+    private service: RegistrationService,
+    private router: Router) {
+      this.myForm = fb.group({
+        'name': ['', Validators.required],
+        'password': ['', Validators.compose([
+          Validators.required, Validators.minLength(8)
+        ]
+        )],
+        'passwordConfirm': ['', Validators.compose([
+          Validators.required, Validators.minLength(8),
+          
+        ])]
+      }, {validator: matchValue})
+      this.name = this.myForm.controls['name'];
+      this.password = this.myForm.controls['password'];
+      this.passwordConfirm = this.myForm.controls['passwordConfirm'];
+   }
+
+   registerUser = (): void => {
+     if (!this.myForm.valid) {
+       this.message = "Form is not valid!";
+       setTimeout(() => {
+        this.message = "";
+      },2000)
+       return;
+     }
+     this.service.registerUser(this.myForm.value).subscribe(
+       (data: string) => {
+         console.log(data);
+         this.router.navigate(['/login'])
+       },
+       (err) => {
+         this.message = "Account already exists!";
+         setTimeout(() => {
+           this.message = "";
+         },2000)
+       }
+     )
+   }
+  ngOnInit(): void { 
+  }
+}
