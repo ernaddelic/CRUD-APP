5e85075373a59d0024ae9afde6acd057a7b8f46f
ernaddelic <ernad.delic77@gmail.com>
2020-01-02 11:58:00 +0100

    new material module

---
+++ angular/src/app/material/material.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatMenuModule} from '@angular/material/menu';
+import { 
+  MatInputModule, 
+  MatOptionModule, 
+  MatSelectModule,
+  MatCardModule,
+  MatTabsModule,
+  MatCheckboxModule,
+  MatIconModule
+  } from '@angular/material';
+
+  const MaterialComponents = [
+    MatInputModule, 
+    MatOptionModule, 
+    MatSelectModule,
+    MatCardModule,
+    MatTabsModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatTableModule,
+    MatTooltipModule,
+    MatMenuModule
+  ]
+
+@NgModule({
+  imports: [
+    MaterialComponents  
+  ],
+  exports: [
+    MaterialComponents
+  ]
+})
+export class MaterialModule { }
