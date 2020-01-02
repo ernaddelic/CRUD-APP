1b344580443325c2554108d664fb4f34352156a1
ernaddelic <ernad.delic77@gmail.com>
2020-01-02 12:32:13 +0100

    new email column added to loguser table

--- spring/src/main/resources/data.sql
+++ spring/src/main/resources/data.sql
@@ -3,5 +3,5 @@ INSERT INTO users(first_name,last_name,age) VALUES("John", "Smith", 27);
 INSERT INTO users(first_name,last_name,age) VALUES("Tim", "Handry", 35);
 INSERT INTO role(name) VALUES("ROLE_ADMIN");
 INSERT INTO role(name) VALUES("ROLE_USER");
-INSERT INTO loguser(name,password) VALUES("admin", "$2y$12$bfbf.T7.x6jdLeqjAFGJR.R7thY9/6kj2GPSTnP3a30MGTqdrj6uq");
+INSERT INTO loguser(name, email, password) VALUES("admin", "admin@gmail.com", "$2y$12$bfbf.T7.x6jdLeqjAFGJR.R7thY9/6kj2GPSTnP3a30MGTqdrj6uq");
 INSERT INTO loguser_roles(user_id,role_id) VALUES(1,1);
