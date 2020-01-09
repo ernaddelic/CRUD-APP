00225f42a8ddcc4a1887037cef6c5f78844440fb
ernaddelic <ernad.delic77@gmail.com>
2020-01-09 13:57:27 +0100

    new fields for User model

--- spring/src/main/java/com/example/Spring/model/User.java
+++ spring/src/main/java/com/example/Spring/model/User.java
@@ -1,5 +1,7 @@
 package com.example.Spring.model;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,57 +16,27 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    private String firstName;
-    private String lastName;
-    private int age;
+    private String fullName;
+    private String email;
+    private String mobile;
+    private String city;
+    private String gender;
+    private Date hireDate;
+    private Boolean isPermanent;
 
     public User() {
     }
 
-    public User(int id, String firstName, String lastName, int age) {
-        this.setId(id);
-        this.setFirstName(firstName);
-        this.setLastName(lastName);
-        this.setAge(age);
-    }
-
-    public User(String firstName, String lastName, int age) {
-        this.setFirstName(firstName);
-        this.setLastName(lastName);
-        this.setAge(age);
-    }
+    
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
     }
 
     public int getId() {
         return id;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public int getAge() {
-        return age;
-    }
 
     @Override
     public boolean equals(Object obj) {
@@ -79,6 +51,89 @@ public class User {
     public int hashCode() {
         return id;
     }
-    
-    
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(Date hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public Boolean getIsPermanent() {
+        return isPermanent;
+    }
+
+    public void setIsPermanent(Boolean isPermanent) {
+        this.isPermanent = isPermanent;
+    }
+
+    public User(int id, 
+            String fullName, 
+            String email, 
+            String mobile, 
+            String city, 
+            String gender, 
+            Date hireDate,
+            Boolean isPermanent) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.mobile = mobile;
+        this.city = city;
+        this.gender = gender;
+        this.hireDate = hireDate;
+        this.isPermanent = isPermanent;
+    }
+
+    public User(String fullName, String email, String mobile, String city, String gender, Date hireDate,
+            Boolean isPermanent) {
+        this.fullName = fullName;
+        this.email = email;
+        this.mobile = mobile;
+        this.city = city;
+        this.gender = gender;
+        this.hireDate = hireDate;
+        this.isPermanent = isPermanent;
+    }
 }
